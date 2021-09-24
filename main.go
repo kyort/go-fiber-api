ed8728731c09308ff6c94879227806fe0d71b80a
kyort <kyort@att.net>
2021-09-24 10:55:15 -0500

    initial upload

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/kyort/go-fiber-api/book"
+	"github.com/kyort/go-fiber-api/database"
+	"gorm.io/driver/sqlite"
+	_ "gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func setRoutes(app *fiber.App) {
+	app.Get("/api/v1/book", book.GetBooks)
+	app.Get("/api/v1/book/:id", book.GetBook)
+	app.Post("/api/v1/book", book.NewBook)
+	app.Delete("/api/v1/book/:id", book.DeleteBook)
+}
+
+func initDatabase() {
+	var err error
+	database.DBConn, err = gorm.Open(sqlite.Open("books.db"))
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+	fmt.Println("Database connection successfully opened")
+
+	database.DBConn.AutoMigrate(&book.Book{})
+	fmt.Println("Database migrated")
+}
+
+func main() {
+	app := fiber.New()
+	initDatabase()
+
+	setRoutes(app)
+
+	app.Listen(":3000")
+}
